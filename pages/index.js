6289bc284e52e2fb7090d4f28c115fd29fdfeec3
CodeGenie.dev <adrian@codegenie.dev>
2022-10-24 06:17:50 +0100

    eslint setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+);
+
+export default Home;
