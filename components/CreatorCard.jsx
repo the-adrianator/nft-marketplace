073f6057282953ba2c17a5fa51a6cfe1af541a23
CodeGenie.dev <adrian@codegenie.dev>
2022-10-25 13:40:38 +0100

    Top Bids section

--- components/CreatorCard.jsx
+++ components/CreatorCard.jsx
@@ -31,7 +31,7 @@ const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => (
 
     <div className="mt-3 minlg:mt-7 text-center flexCenter flex-col">
       <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">{creatorName}</p>
-      <p className="mt-1 font-poppins dark:text-white text-nft-black-1 font-semibold text-base">{creatorEths.toFixed(2)} <span className="font-normal">ETH</span></p>
+      <p className="mt-1 font-poppins dark:text-white text-nft-black-1 font-semibold text-base">{creatorEths} <span className="font-normal">ETH</span></p>
     </div>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 
 import images from '../assets';
 import { makeId } from '../utils/makeId';
@@ -65,7 +65,7 @@ const Home = () => {
                   rank={i}
                   creatorImage={images[`creator${i}`]}
                   creatorName={`0x${makeId(3)}...${makeId(4)}`}
-                  creatorEths={10 - i * 0.5}
+                  creatorEths={(10 - i * 0.32).toFixed(2)}
                 />
               ))}
               {!hideButtons && (
@@ -79,6 +79,28 @@ const Home = () => {
                 </>
               )}
             </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 first:before:font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Top Bids</h1>
+            <div>SearchBar</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${1}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  onwer: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
