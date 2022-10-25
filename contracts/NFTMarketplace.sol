67187dd24373685ff2c690ca5760bf685a2a2cc5
CodeGenie.dev <adrian@codegenie.dev>
2022-10-25 18:37:20 +0100

    Function CreateMarketItem

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -61,4 +61,21 @@ contract NFTMarketplace is ERC721URIStorage {
 
     return newTokenId;
   }
+
+  function createMarketItem(uint256 tokenId, uint256 price) private {
+    require(price > 0, "Price must be at least 1");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
+
+    idToMarketItem[tokenId] = MarketItem(
+      tokenId,
+      payable(msg.sender),
+      payable(address(this)),
+      price,
+      false
+    );
+
+    _transfer(msg.sender, address(this), tokenId);
+
+    emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
+  }
 }
