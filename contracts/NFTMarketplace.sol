84f0c2cd5079cab217803a3a12a953afb0d19dc6
CodeGenie.dev <adrian@codegenie.dev>
2022-10-25 18:17:47 +0100

    Update and Get listing price functions

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -38,4 +38,14 @@ contract NFTMarketplace is ERC721URIStorage {
   constructor() {
     owner = payable(msg.sender);
   }
+
+  function updateListingPrice(uint _listingPrice) public payable {
+    require(owner == msg.sender, "Only marketplace owner can update the listing price");
+
+    listingPrice = _listingPrice;
+  }
+
+  function getListingPrice() public view returns (uint256) {
+    return listingPrice;
+  }
 }
