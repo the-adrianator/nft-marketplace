b1aad9e00139de3a7f1d4173c3a10476c35f7c82
CodeGenie.dev <adrian@codegenie.dev>
2022-10-28 21:43:07 +0100

    Fetching NFTs

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [dedicatedEndPoint, 'nft-bucket.infura-ipfs.io'],
+    domains: ['nft-bucket.infura-ipfs.io'],
   },
   // env: {
   //   BASE_URL: process.env.BASE_URL,
